1df16a9836eeb5b327cb279f8a593c74c4ca13f0
Michael Rusezera <michaelrusezera@gmail.com>
2025-07-02 12:58:05 +0300

    environment variaBLE

--- server/app.py
+++ server/app.py
@@ -100,4 +100,5 @@ def generate_questions():
         return jsonify({"error": f"Error generating questions: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    
+    app.run(host='0.0.0.0', port=10000)
